b8c1006f611214ba695d20df0b2312b450f3c840
Beichen Wang <beichen.wang@dena.local>
2016-12-13 23:36:53 +0800

    modify

--- usaco/section_2_3/cow_pedigrees/cow_pedigrees_standard.cpp
+++ usaco/section_2_3/cow_pedigrees/cow_pedigrees_standard.cpp
@@ -1,4 +1,4 @@
-include <cstdio>
+#include <cstdio>
 #include <cstdlib>
 #include <cassert>
 #define MOD 9901
@@ -16,19 +16,18 @@ int main() {
     for (int i=2;i<=K;i++) {
         for (int j=1;j<=N;j+=2)
             for (int k=1;k<=j-1-k;k+=2) {
-                if (k!=j-1-k) c=2; else c=1;    
+                if (k!=j-1-k) c=2; else c=1; 
+                if (i == K && j == N)
+                printf("%d %d %d %d\n",i,j,k,c );   
                 table[i][j]+=c*(
                         smalltrees[i-2][k]*table[i-1][j-1-k]  // left subtree smaller than i-1
                         +table[i-1][k]*smalltrees[i-2][j-1-k]  // right smaller
                         +table[i-1][k]*table[i-1][j-1-k]);// both i-1
                 table[i][j]%=MOD;
             }
-        for (int k=0;k<=N;k++) {          // we ensure that smalltrees[i-2][j] in
-the next i
-            smalltrees[i-1][k]+=table[i-1][k]+smalltrees[i-2][k]; // iteration
-contains the number
-            smalltrees[i-1][k]%=MOD;           // of trees smaller than i-1 and with
-j nodes
+        for (int k=0;k<=N;k++) {          // we ensure that smalltrees[i-2][j] in the next i
+            smalltrees[i-1][k]+=table[i-1][k]+smalltrees[i-2][k]; // iteration contains the number
+            smalltrees[i-1][k]%=MOD;           // of trees smaller than i-1 and with j nodes
         }
     }
     
