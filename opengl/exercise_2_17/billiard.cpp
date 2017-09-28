8bbecb8c42a3c04cfbe29697afe6ee50f1e5463c
admin <admin@huamingdeiMac.local>
2017-09-28 21:02:21 +0800

    2-17 再上传一版

--- opengl/exercise_2_17/billiard.cpp
+++ opengl/exercise_2_17/billiard.cpp
@@ -6,6 +6,16 @@ const GLfloat whiteLight[] = {0.8,0.8,0.8,1.0};
 GLfloat matSpecular [] = {0.3,0.3,0.3,1.0};  
 GLfloat matShininess [] = {20.0};  
 GLfloat matEmission [] = {0.3,0.3,0.3,1.0};  
+
+GLfloat v1[] = {0.5,0.5};
+GLfloat v2[] = {0.0,0.0};
+
+GLfloat p1[] = {0.0,5.0};
+GLfloat p2[] = {0.0,-5.0};
+
+GLfloat r = 1.0;
+
+GLfloat dt = 0.01;
 void init()  
 {  
     glClearColor(0.3,0.3,0.3,1.0);  
@@ -30,27 +40,48 @@ void display()
     glMatrixMode(GL_MODELVIEW);  
     glPushMatrix(); 
  
-    glTranslatef(0.0, 5.0, 0.0);
+    glTranslatef(p1[0], p1[1], 0.0);
     glMaterialfv(GL_FRONT,GL_SPECULAR,matSpecular);  
     glMaterialfv(GL_FRONT,GL_SHININESS,matShininess);  
     glMaterialfv(GL_FRONT,GL_EMISSION,matEmission);  
-    glutSolidSphere(1.0,16,16);  
-    glTranslatef(0.0, -5.0, 0.0);
+    glutSolidSphere(r,16,16);  
+    glTranslatef(-p1[0], -p1[1], 0.0);
 
-    glTranslatef(0.0, -5.0, 0.0);
+    glTranslatef(p2[0], p2[1], 0.0);
     glMaterialfv(GL_FRONT,GL_SPECULAR,matSpecular);  
     glMaterialfv(GL_FRONT,GL_SHININESS,matShininess);  
     glMaterialfv(GL_FRONT,GL_EMISSION,matEmission);  
-    glutSolidSphere(1.0,16,16);  
-    glTranslatef(0.0, 5.0, 0.0);
+    glutSolidSphere(r,16,16);  
+    glTranslatef(-p2[0], -p2[1], 0.0);
     glPopMatrix();  
 
     glFlush();  
 }
+
+void move()
+{
+    p1[0] = p1[0] + v1[0]*dt;
+    p1[1] = p1[1] + v1[1]*dt;
+
+    p2[0] = p2[0] + v2[0]*dt;
+    p2[1] = p2[1] + v2[1]*dt;    
+}
   
 void checkCollision()
 {
+    //collide with ball
+    GLfloat centerDis = sqrt((p2[0]-p1[0])*(p2[0]-p1[0])+(p2[1]-p1[1])*(p2[1]-p1[1]));
+    if (centerDis < 2*r){
+        
+    }
+    //collide with wall 
+}
 
+void renderScene()
+{
+   move();
+   checkCollision();
+   display();
 }
 
 void reshape(int w,int h)  
@@ -68,7 +99,7 @@ int main(int argc,char *argv[])
     glutCreateWindow("ball");  
     glutDisplayFunc(display);  
     glutReshapeFunc(reshape);  
-//    glutIdleFunc(renderScene);
+    glutIdleFunc(renderScene);
     init();  
     glutMainLoop();  
     return EXIT_SUCCESS;  
